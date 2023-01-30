3fb79540682bf5792a71c9bd2c46ff95ecafce53
dk <omnipresent9@yandex.ru>
2023-01-30 20:20:32 +0300

    Добавил новый шрифт

--- app/src/main/java/com/dk/nasa/ui/main/AppState.kt
+++ app/src/main/java/com/dk/nasa/ui/main/AppState.kt
@@ -3,7 +3,7 @@ package com.dk.nasa.ui.main
 import com.dk.nasa.model.PictureOfTheDayData
 
 sealed class AppState {
-    data class Success(val pictureOfTheDayData: PictureOfTheDayData):AppState()
-    data class Error(val message: String):AppState()
-    object Loading:AppState()
+    data class Success(val pictureOfTheDayData: PictureOfTheDayData) : AppState()
+    data class Error(val message: String) : AppState()
+    object Loading : AppState()
 }
