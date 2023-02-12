5ca7dfe9e661a53ac61eacb40abc84d215277b25
dk <omnipresent9@yandex.ru>
2023-02-12 20:13:36 +0300

    добавил исчезновение фотографии при открытии бехейвера

--- app/src/main/java/com/dk/nasa/ui/main/MainFragment.kt
+++ app/src/main/java/com/dk/nasa/ui/main/MainFragment.kt
@@ -8,12 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.dk.nasa.R
 import com.dk.nasa.databinding.FragmentMainBinding
 import com.dk.nasa.model.pictureOfTheDay.PictureOfTheDayData
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainFragment : Fragment() {
@@ -24,10 +26,10 @@ class MainFragment : Fragment() {
     }
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater)
         return binding.root
@@ -58,6 +60,19 @@ class MainFragment : Fragment() {
             }
         }
 
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet.bottomSheetContainer)
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+            }
+
+            @RequiresApi(Build.VERSION_CODES.Q)
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                binding.pictureOfTheDay.transitionAlpha = 1 - slideOffset
+            }
+
+        })
+
         viewModel.getLiveData().observe(viewLifecycleOwner) { appState ->
             when (appState) {
                 is AppState.Error -> {
@@ -75,8 +90,7 @@ class MainFragment : Fragment() {
 
         binding.inputLayout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data =
-                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputET.text.toString()}")
+                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputET.text.toString()}")
             })
         }
     }
@@ -107,13 +121,13 @@ class MainFragment : Fragment() {
 
     private fun setHD(isHD: Boolean, pictureOfTheDayData: PictureOfTheDayData) {
         if (isHD) {
-            with(binding){
+            with(binding) {
                 chipHD.text = getString(R.string.hd)
                 pictureOfTheDay.load(pictureOfTheDayData.hdurl)
             }
 
         } else {
-            with(binding){
+            with(binding) {
                 chipHD.text = getString(R.string.sd)
                 pictureOfTheDay.load(pictureOfTheDayData.url)
             }
